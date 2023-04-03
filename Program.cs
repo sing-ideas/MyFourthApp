fa1c66d9978d1ef6be187c0bb685c4b68cd1d2f0
Singaravelu Souppourayen <34727666+sing-ideas@users.noreply.github.com>
2023-04-04 00:23:19 +0530

    how this goes to the VS 2022
    either by sync, pull fetch

--- Program.cs
+++ Program.cs
@@ -2,3 +2,4 @@
 Console.WriteLine("Hello, World!");
 // hope this time it works
 // edited in the github browser, need to see this change done in browser into the vs 2022 by pull or fetch
+//I saw the above line in VS 2022, not sure how - either by sync or by fetch or by pull.
