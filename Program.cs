870f0cb94538f0af51809acee0b2b95e15977e1d
Singaravelu Souppourayen <34727666+sing-ideas@users.noreply.github.com>
2023-04-04 00:25:04 +0530

    pull worked, testing fetch

--- Program.cs
+++ Program.cs
@@ -3,3 +3,5 @@ Console.WriteLine("Hello, World!");
 // hope this time it works
 // edited in the github browser, need to see this change done in browser into the vs 2022 by pull or fetch
 //I saw the above line in VS 2022, not sure how - either by sync or by fetch or by pull.
+// I clicked the pull the above line is reflecting in the VS 2022 
+// how about fetch what is the difference.
